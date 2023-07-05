2375faff2854e3fe687fb151bb002530786f28a7
e-LisaGoodman <thinkletoes@gmail.com>
2023-07-05 13:58:53 -0400

    Color and Placement

--- Labs/02-3/js Third Example/app.js
+++ Labs/02-3/js Third Example/app.js
@@ -2,7 +2,7 @@
 //Make a circle look like it is traveling from left to right and looping back in from the left.
 
 //Make an application that starts drawing a ball on the left hand of the screen
-let xPos = 0; 
+let xPos = -100; 
 
 //Create a canvas 800px x 600 px.
 
@@ -14,15 +14,15 @@ function draw(){
     background(0)
     //- make it move to the right 5 pixels per frame.
     xPos += 5;
-    fill(111,111,22)
+    fill(200,200,0)
     arc(xPos,300,200,200,QUARTER_PI,320)
     //for fun- Eating motion upon mouse click
     if(mouseIsPressed == true){
         arc(xPos,300,200,200,radians(5),radians(355))
     }
     //Write the code so that when the circle reaches 800 pixels to the right, 
-    if(xPos > 799){
+    if(xPos > 800){
         //move the circle to the far left of the screen.
-        xPos = 1
+        xPos = -100
     }
 }
