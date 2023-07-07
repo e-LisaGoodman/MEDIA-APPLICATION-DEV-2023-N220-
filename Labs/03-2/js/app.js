aeebbfe30ea4e5c5d571fcbaf809314894b89ad3
e-LisaGoodman <thinkletoes@gmail.com>
2023-07-07 13:18:08 -0400

    Updates and Lab 3-3

--- Labs/03-2/js/app.js
+++ Labs/03-2/js/app.js
@@ -10,29 +10,32 @@ function setup() {
 function draw() {
     background (222)
     noStroke()
-    fill(222,0,0)
 
 //Build 4 rows with increasing number of squares per column in each row 
    //Top row has just one far left square
     for(y=0;y<50;y+=52){
         for(x=0;x<50;x+=52){
+            fill(0,111,0)
             square(x,y,50)
         }
         //Sencond row from top has two far left squares
         for(y=52;y<100;y+=52){
             for(x=0;x<100;x+=52){
+                fill(222,140,25)
                 square(x,y,50)
             }
         }
         //Third row from top has three far left squares
         for(y=104;y<150;y+=52){
             for(x=0;x<150;x+=52){
+                fill(222,50,0)
                 square(x,y,50)
             }
         }
         //Fourth row from top has four far left squares
         for(y=156;y<200;y+=52){
             for(x=0;x<200;x+=52){
+                fill(170,0,0)
                 square(x,y,50)
             }
         }
--- Labs/03-3/js/app.js
+++ Labs/03-3/js/app.js
@@ -1,3 +1,20 @@
+//Concentric Circles
+//Define the visible area
 function setup() {
     createCanvas(800,600)
+}
+//Begin the drawing process
+function draw(){
+    background(111)
+    //noFill()
+    strokeWeight(10)
+    //Initiate loop to repeat circles within circles
+    for (i=10; i>0;i--){
+        //Gradient stroke color per loop
+        stroke(255-(i*20),0,0)
+        //Gradient fill color per loop
+        fill(0,0,255-(i*25))
+        //Draw 1 circle per loop
+        circle(400, 300, i*55)
+    }
 }
