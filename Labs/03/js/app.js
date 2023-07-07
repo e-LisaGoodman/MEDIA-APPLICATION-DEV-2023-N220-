8976b10f35e390aa31ea799d928a1b21c4b84816
e-LisaGoodman <thinkletoes@gmail.com>
2023-07-07 14:39:24 -0400

    Updates

--- Labs/03/js/app.js
+++ Labs/03/js/app.js
@@ -9,22 +9,23 @@ function draw() {
     background(200);
     var y = 300;
     // Draw/iterate 25 shapes where if no criteria are met, they are black circles
+    noStroke()
     for (i = 1; i < 26; ++ i) {
         let x = ((i*32)-32); 
 
         //When the iterand is divisible by 3 AND 5, draw a blue square instead *should only be the 15th shape
         if (i % 3 == 0 && i % 5 == 0) {
-            fill(0, 0, 255);
+            fill(0, 0, 200);
             square(x, y, 32);
         }
         //When the iterand is divisible by 5, draw a green square instead 
         else if (i % 5 == 0) {
-            fill(0, 255, 0);
+            fill(45, 180, 45);
             square(x, y, 32);
         }
         //When the iterand is divisible by 3, draw a purple circle instead 
         else if (i % 3 == 0) {
-            fill(150, 10, 200);
+            fill(135, 0, 180);
             ellipse(x+16, y+16, 32, 32);
         }
         //Default when iterand does not meet criteria; black circle
