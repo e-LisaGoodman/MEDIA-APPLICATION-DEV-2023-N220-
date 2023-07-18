7c4ce943fdf4efcd17108a5006233c406bfb66b7
e-LisaGoodman <thinkletoes@gmail.com>
2023-07-18 18:12:28 -0400

    Lab Corrections

--- Labs/06/js/app.js
+++ Labs/06/js/app.js
@@ -1,17 +1,27 @@
 //Data-Driven Display
-//Make an object with all the properties for a graphic on the screen - 
-//width, height, color, position. 
+let obj = {
+width: 50,
+height: 50,
+color: "#00FF00",
+x: 400,
+y: 300, 
+}
 
 
 
-//Canvas size: 400x300
+let nameSize = document.getElementByID("UserName")
 function setup(){
     createCanvas(800,600)
-    noStroke()
 }
-//In your create or draw function, draw that object using the data in your object.
+
 function draw(){
-    background(0)
-    fill("#ff6666")
-    circle(400,300,mouseX,mouseY)
+  rect(obj.x, obj.y, obj.width, obj.height)
 }
+
+//In your create or draw function
+//function namerOlogy(){
+  //  let nameSize = Number(UserName.value);
+    //Make an object with all the properties for a graphic on the screen - 
+    //draw that object using the data in your object
+    //UserName.value = "";
+//}
