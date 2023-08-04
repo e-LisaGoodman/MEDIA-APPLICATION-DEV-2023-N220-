e810c5f3b16a363c55c09c0633e284368745fefb
e-LisaGoodman <thinkletoes@gmail.com>
2023-08-04 14:03:44 -0400

    Final Labs started

--- Labs/10-3/js/app.js
+++ Labs/10-3/js/app.js
@@ -3,3 +3,16 @@
 //Each button should have a data-answer attribute that lists the answer.
 //When a button is clicked, display the answer to the button's question in a div. 
 //Use only one function- function must make use of the button's data-attribute.
+
+let element = document.getElementById("flashCard");
+element.addEventListener("click", itsClicked);
+
+function itsClicked(event){
+    event.target.style.backgroundColor = "#ff9900";
+    event.target.style.width = "200px";
+    event.target.style.height = "40px";
+    event.target.style.margin = "5px";
+    let response = event.target.getAttribute("data-response");
+    event.target.innerHTML = response;
+    event.target.innerHTML.right = "50px";
+}
